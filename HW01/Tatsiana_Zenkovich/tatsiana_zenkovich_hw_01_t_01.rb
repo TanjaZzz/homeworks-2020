80c4d3f76bd770d9d629f2a98e68137c8a8aca80
TanjaZzz <51056474+TanjaZzz@users.noreply.github.com>
2020-03-18 19:34:45 +0300

    Update tatsiana_zenkovich_hw_01_t_01.rb

--- HW01/Tatsiana_Zenkovich/tatsiana_zenkovich_hw_01_t_01.rb
+++ HW01/Tatsiana_Zenkovich/tatsiana_zenkovich_hw_01_t_01.rb
@@ -4,5 +4,4 @@ h = gets.chomp.to_i
 print 'Type minutes'
 m = gets.chomp.to_i
 puts 'Awesome! Let me work hard instead of you!'
-angle = (30 * h - 5.5 * m).abs.to_s
-puts 'The angle between #{h} hours and #{m} minutes hands is #{angle} degrees'
+puts "The angle between #{h} hours and #{m} minutes hands is #{(30 * h - 5.5 * m).abs.to_s} degrees"
