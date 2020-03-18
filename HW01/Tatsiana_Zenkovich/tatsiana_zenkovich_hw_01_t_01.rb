4c433fd431bd1922108fcef256c066edd4c58adb
TanjaZzz <51056474+TanjaZzz@users.noreply.github.com>
2020-03-18 19:25:20 +0300

    Update tatsiana_zenkovich_hw_01_t_01.rb

--- HW01/Tatsiana_Zenkovich/tatsiana_zenkovich_hw_01_t_01.rb
+++ HW01/Tatsiana_Zenkovich/tatsiana_zenkovich_hw_01_t_01.rb
@@ -1,8 +1,8 @@
-puts "Welcome to my first program!"
-print "Type hours"
+puts 'Welcome to my first program!'
+print 'Type hours'
 h = gets.chomp.to_i
 print "Type minutes"
 m = gets.chomp.to_i
-puts "Awesome! Let me work hard instead of you!"
-angle= (30*h-5.5*m).abs.to_s
-puts "The angle between #{h} hours and #{m} minutes hands is #{angle} degrees"
+puts 'Awesome! Let me work hard instead of you!'
+angle = (30 * h - 5.5 * m).abs.to_s
+puts 'The angle between #{h} hours and #{m} minutes hands is #{angle} degrees'
