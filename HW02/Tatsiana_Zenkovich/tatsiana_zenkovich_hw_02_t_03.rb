6528015eef75e4c08816a7991a22aaed325dc09f
TanjaZzz <x-strange@yandex.ru>
2020-03-22 16:28:34 +0300

    Add comments about the code

--- HW02/Tatsiana_Zenkovich/tatsiana_zenkovich_hw_02_t_03.rb
+++ HW02/Tatsiana_Zenkovich/tatsiana_zenkovich_hw_02_t_03.rb
@@ -13,10 +13,14 @@ def task_3()
     end
   end
 
+# check if the log contains any Calling core with action
+
 if relevant_line == []
   puts "0"
   exit
 end
+
+# count time for each Calling core with action
 
 time_between = []
 names = []
@@ -33,32 +37,48 @@ relevant_line.each do |new_line|
   microsec = time.split('.').last.to_i
   seconds = seconds + microsec/10.0
   data1 = new_line.split('-')[2][1]
-
+     
+     # check if it's time over midnight between actions
+    
     if data0 == data1 
       t1 = seconds + minute*60 + hour*60*60
     else
       t1 = seconds + minute*60 + hour*60*60 + 86400
     end
 
+    # count time between actions
+
   data1 = data0
   time_difference = (t1 - t0).round(1)
 
   t0 = t1
+
+  # save names of actions in array and time between actions in another array
+
   name = new_line.split(' ').last
 
   time_between << time_difference
   names << name
 
 end
+
+# check if there was only one Calling core with action 
+
 time_between.delete_at(0)
   if time_between == []
     time_between = [0]
   end
+
+# create part of output   
+
 time_between.collect!{|x| x.to_s+" seconds between calling core with action "}
 
+# create additional array 
 
 names_for_array = names.clone
 names.shift
+
+# create and array with info about 2 actions and time between them
 
 final_array = time_between.zip(names_for_array, names)
 puts final_array
