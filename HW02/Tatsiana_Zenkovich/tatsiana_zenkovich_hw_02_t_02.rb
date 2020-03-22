2cd6226a211b35ecf224a382942556ecad807958
TanjaZzz <x-strange@yandex.ru>
2020-03-22 17:55:13 +0300

    Add corrections from rubocop

--- HW02/Tatsiana_Zenkovich/tatsiana_zenkovich_hw_02_t_02.rb
+++ HW02/Tatsiana_Zenkovich/tatsiana_zenkovich_hw_02_t_02.rb
@@ -2,7 +2,7 @@
 
 lines = []
 
-File.open ('log.txt') do |review_file|
+File.open('log.txt') do |review_file|
   lines = review_file.readlines
 end
 
@@ -11,6 +11,6 @@ lines.each do |line|
   if line =~ %r{(\d+\.\d+\.\d+\.\d+)\s\-\s\-\s\[(\d+/\w+/\d{4}\:\d{2}\:\d{2}\:\d{2}\s\+\d+)\]\s\W\w+(\s\W\w+\W\d+\W\w+)\s\w+/\d+\.\d+\W\s(.*)}
     line = "\"#{Regexp.last_match(2)} FROM: #{Regexp.last_match(1)} TO: #{Regexp.last_match(3).upcase}\""
     relevant_line << line
-end
+  end
 end
 puts relevant_line
