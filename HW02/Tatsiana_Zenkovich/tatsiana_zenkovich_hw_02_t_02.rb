f6860986b23d2824f6c7273712b5abbf16fae598
TanjaZzz <x-strange@yandex.ru>
2020-03-24 23:56:04 +0300

    Change variables to CONSTANTS

--- HW02/Tatsiana_Zenkovich/tatsiana_zenkovich_hw_02_t_02.rb
+++ HW02/Tatsiana_Zenkovich/tatsiana_zenkovich_hw_02_t_02.rb
@@ -1,18 +1,16 @@
-# frozen_string_literal: true
-
 lines = []
 
 File.open('log.txt') do |review_file|
   lines = review_file.readlines
 end
 
-re = []
+found = []
 lines.each do |i|
-  ip = i[/\d+\.\d+\.\d+\.\d+/]
-  date = i[%r{\[\d+/\w+/\d{4}\:\d{2}\:\d{2}\:\d{2}\s\+\d+\]}]
-  adr = i[/T .*H/]
-  if ip && date && adr
-    re << date[1..-1] + 'FROM:' + ip[0..0] + 'TO:' + adr[1..-3].upcase
+  IP = i[/\d+\.\d+\.\d+\.\d+/]
+  DATE = i[%r{\[\d+/\w+/\d{4}\:\d{2}\:\d{2}\:\d{2}\s\+\d+\]}]
+  ADDRESS = i[/T .*H/]
+  if IP && DATE && ADDRESS
+    found << DATE[1..-1] + 'FROM:' + IP[0..0] + 'TO:' + ADDRESS[1..-3].upcase
   end
 end
-puts re
+puts found
