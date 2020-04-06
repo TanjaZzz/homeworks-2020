4b7e01f8088909ccb12322cb9a2e11811fb2e05e
TanjaZzz <x-strange@yandex.ru>
2020-04-06 23:50:32 +0300

    Correct broken code after rubocop

--- HW04/Tatsiana_Zenkovich/spec/my_methods.rb
+++ HW04/Tatsiana_Zenkovich/spec/my_methods.rb
@@ -1,7 +1,8 @@
 module MyArray
+  # rubocop:disable Style/For: Prefer each over for
   def my_each
     if block_given?
-      (0..length - 1).each do |i|
+      for i in (0..length - 1)
         proc.call(self[i])
       end
       return self
@@ -12,8 +13,8 @@ module MyArray
   def my_map
     results = []
     if block_given?
-
-      (0..length - 1).each do |i|
+      for i in (0..length - 1)
+        proc.call(self[i])
         results << proc.call(self[i])
       end
       return results
@@ -24,13 +25,15 @@ module MyArray
   def my_select
     if block_given?
       results = []
-      (0..length - 1).each do |i|
+      for i in (0..length - 1)
+        proc.call(self[i])
         results << self[i] if proc.call(self[i]) == true
       end
       return results
     end
     return to_enum(:my_select) unless block_given?
   end
+  # rubocop:enable Style/For: Prefer each over for
 end
 
 class Array
